2f888eeda23d4648b0407a8089c30e3d2c33e7c4
Wembley G. Leach, Jr <wembley.gl@gmail.com>
2017-12-30 00:40:26 -0500

    Add support for webpack, babel, and render basic react component

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+    <p>Watchlist</p>,
+    document.getElementById("react-container")
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,28 @@
-module.exports = {
-    entry: "./src/index.js"
+const path = require("path")
+
+const config = {
+    entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
+        path: path.resolve(__dirname, "dist/assets"),
+        filename: "bundle.js",
+        publicPath: "/assets/"
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist"),
+        port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["env", "react"]
+                }
+            }
+        ]
     }
 }
+
+module.exports = config
