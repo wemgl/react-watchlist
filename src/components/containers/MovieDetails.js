81317a8388ce763cf0e3bc7ae00bab3e610dce6f
Wembley G. Leach, Jr <wembley.gl@gmail.com>
2018-01-09 19:59:42 -0500

    Fix access to router match params in MovieDetails

--- src/components/containers/MovieDetails.js
+++ src/components/containers/MovieDetails.js
@@ -1,9 +1,8 @@
 import {connect} from 'react-redux'
-import {withRouter} from 'react-router-dom'
 import MovieDetails from '../ui/MovieDetails'
 
 const mapStateToProps = (state, props) => ({
     match: props.match
 })
 
-export default withRouter(connect(mapStateToProps)(MovieDetails))
+export default connect(mapStateToProps)(MovieDetails)
--- src/components/ui/MovieDetails.js
+++ src/components/ui/MovieDetails.js
@@ -1,7 +1,11 @@
 import React from "react"
 
 export default class MovieDetails extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
     render() {
-        return <p>Movie Details {this.props.match.imdbID}</p>
+        return <p>Movie Details {this.props.match.params.imdbID}</p>
     }
 }
