c80bba92e98c55dc4eabcb409b20c44cd4634f09
Wembley G. Leach, Jr <wembley.gl@gmail.com>
2018-01-13 07:59:50 -0500

    Commit Jest snapshots to version control

--- src/store/index.js
+++ src/store/index.js
@@ -2,4 +2,5 @@ import thunk from 'redux-thunk';
 import { applyMiddleware, createStore } from 'redux';
 import appReducer from './reducers';
 
-export default (initialState = {}) => applyMiddleware(thunk)(createStore)(appReducer, initialState)
+export default (initialState = {}) =>
+  applyMiddleware(thunk)(createStore)(appReducer, initialState)
